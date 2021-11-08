8c81a2158162277591a50a15986c7765fa3cd338
Yuzukeik <andy1999@gmail.com>
2021-11-08 01:38:02 -0800

    code cleanup, added simple UI

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,71 +1,106 @@
-let playerInput = []
 let computerMove = [];
-let scoring = { cpuScore : 0, playerScore : 0, totalRounds : 0};
-
-function capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1);
-}
+let scoring = { cpuScore : 0, playerScore : 0, totalRounds = 0 };
+let roundLength = 5;
 
 
-function playerMoveCheck() {
 
-    playerInput = prompt('Enter "rock", "paper", or "scissors"').toLowerCase();
-    if (playerInput == 'rock' || playerInput == 'paper' || playerInput == 'scissors') {
-        return playerInput;
-    } else {
-        console.log('Invalid input from playerMoveCheck');
-        playerMoveCheck();
-    }
-}
 
 
 function cpuMove() { 
 
     let rng = Math.random() * Math.floor(3);
     if (rng < 1) {    
-        computerMove = "rock";
+        computerMove = "Rock";
     }   else if (rng < 2) {
-        computerMove = "paper";
+        computerMove = "Paper";
     }   else {
-        computerMove = "scissors";
+        computerMove = "Scissors";
     } return computerMove;
+
 }
 
 
-function rockPaperScissors() {
-    
-    playerMoveCheck(); 
+function rockPaperScissors(playerInput) {
+    if (playerInput == undefined) return;
+    console.log({playerInput});
+
     cpuMove(); 
 
     if (playerInput == computerMove) {
-        console.log(capitalize(playerInput) + " against " + capitalize(computerMove) + ". It's a tie!");
-    } else if (playerInput == 'rock' && computerMove == 'paper') {
+        console.log(playerInput + " against " + computerMove + ". It's a tie!");
+    } else if (playerInput == 'Rock' && computerMove == 'Paper') {
         ++scoring.totalRounds;
-        console.log(capitalize(playerInput) + ' loses to ' + capitalize(computerMove));
-        return scoring.cpuScore++
-    } else if (playerInput == 'paper' && computerMove == 'scissors') {
+        console.log(playerInput + ' loses to ' + computerMove);
+        return score('cpu')
+    } else if (playerInput == 'Paper' && computerMove == 'Scissors') {
         ++scoring.totalRounds;
-        console.log(capitalize(playerInput) + ' loses to ' + capitalize(computerMove));
-        return scoring.cpuScore++;
-    } else if (playerInput == 'scissors' && computerMove == 'rock') {
+        console.log(playerInput + ' loses to ' + computerMove);
+        return score('cpu');
+    } else if (playerInput == 'Scissors' && computerMove == 'Rock') {
         ++scoring.totalRounds;
-        console.log(capitalize(playerInput) + ' loses to ' + capitalize(computerMove));
-        return scoring.cpuScore++;
+        console.log(playerInput + ' loses to ' + computerMove);
+        return score('cpu');
     } else {
         ++scoring.totalRounds;
-        console.log(capitalize(playerInput) + ' beats ' + capitalize(computerMove) + '. You win!');
-        return scoring.playerScore++;
+        console.log(playerInput + ' beats ' + computerMove + '. You win!');
+        return score('player');
     }
 }
 
-function referee(roundLength) {
+
+function gameStart() {
+    roundLength = parseInt(prompt('How many rounds would you like to play for?'));
+    if (Number.isInteger(roundLength)) {
+        return roundLength;
+    } else {
+        alert('Invalid input. Setting to default (3 rounds).')
+        return roundLength = 3;
+    }
     
-    while (scoring.playerScore < roundLength && scoring.cpuScore < roundLength) {
-        rockPaperScissors();
-        console.table(scoring);
-    } 
 }
 
-referee(5);
+function score(winner) {
+    if (winner == 'player') {
+        ++scoring.playerScore;
+        console.table({ scoring });
+    } else {
+        ++scoring.cpuScore;
+        console.table({ scoring });
+    }
+
+    if ((scoring.cpuScore + scoring.playerScore >= roundLength)) {
+        console.log('game over');
+        resetScore();
+        gameStart();
+    }
+
+}
+
+function resetScore() {
+    return scoring = { cpuScore : 0, playerScore : 0, totalRounds : 0};
+}
 
 
+gameStart();
+
+
+const rockInput = document.querySelector('.rock');
+let rockPushCount = 0;
+rockInput.addEventListener('click', () => {
+    rockInput.textContent = 'Rock ' + ++rockPushCount;
+    rockPaperScissors('Rock');
+});
+
+const paperInput = document.querySelector('.paper');
+let paperPushCount = 0;
+paperInput.addEventListener('click', () => {
+    paperInput.textContent = 'Paper ' + ++paperPushCount;
+    rockPaperScissors('Paper');
+});
+
+const scissorsInput = document.querySelector('.scissors');
+let scissorsPushCount = 0;
+scissorsInput.addEventListener('click', () => {
+    scissorsInput.textContent = 'Scissors ' + ++scissorsPushCount;
+    rockPaperScissors('Scissors');
+});
